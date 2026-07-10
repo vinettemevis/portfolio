013a3980bd4d909b35816bc58c035d12cdc256bb
vinettemevis <42896435+vinettemevis@users.noreply.github.com>
2026-07-10 21:37:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
